e323bc675d484afbe92a3123c7754908b02dfbf9
zhangtengjin <1257450210@qq.com>
2020-07-09 09:51:03 +0800

    format

--- README.md
+++ README.md
@@ -3,5 +3,8 @@
 step
 + 通过 create 方法生成 monaco，传入当前 dom 实例和默认 options（语言、编辑器一些配置）
 + monaco-editor-webpack-plugin 可以注入我们所需要的编辑器功能和支持的语言
-+ 拿到 create 方法返回 monaco实例，做一些初始化操作（defineTheme、事件监听）等
++ 拿到 create 方法返回 monaco实例，做一些初始化操作（defineTheme、事件监听）等  
+...
+
+语言注册  
 ...
--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
 import React from 'react';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
 } from "react-router-dom";
 import Editor from './editor';
 export default function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route path="/editor">
-          <Editor />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Switch>
+                <Route path="/editor">
+                    <Editor />
+                </Route>
+                <Route path="/">
+                    <Home />
+                </Route>
+            </Switch>
+        </Router>
+    );
 }
 
 function Home() {
-  return <div>
-    <h2>Welcome Monaco-editor</h2>
-    <Link to='/editor'>editor demo</Link>
-  </div>;
+    return <div>
+        <h2>Welcome Monaco-editor</h2>
+        <Link to='/editor'>editor demo</Link>
+    </div>;
 }
